e27632ebe052ef2f122cabe73b09727932f66086
五十嵐啓太 <keita.igarashi555@gmail.com>
2019-10-01 23:20:08 +0900

    update

---
+++ php01/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+        <footer>    
+            <div class="contact-form">
+                <a id="contact-form"></a>
+                <h3>Contact form</h3>
+                <p>メールアドレス</p>
+                <input type="text">
+                <p>お問い合わせ内容（必須)</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <p>※必須項目は必ずご入力ください</p>
+                <input class="contact-submit" type="submit" value="送信" > 
+            </div>    
+        </footer>
+</html>        
---
+++ php01/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="CSS/stylesheet.css">
+            <title>My Profile</title>
+            <link rel="stylesheet" href="css/font-awesome/css/font-awesome.css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    </head>
+    <body>
+        <header>      
+            <nav> 
+                <ul>
+                    <li><a href="#profile">Profile</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#works">Works</a></li>
+                    <li><a href="#contact-form">Contact Me</a></li>
+                </ul>    
+            </nav>
+        </header>
+        <main>
+        <section class="site-ttl">
+                <h1>Keita Igarashi</h1>
+                <p>~Portfolio site~</p>
+        </section>
+        <section class="profile">
+            <a id="profile"></a>
+            <h2>Profile</h2>
+            <ul>   
+                <li>Live:東京都</li>
+                <li>Age:29</li>
+                <li>Job:IT関係</li>
+                <li>エンジニア職への転職を考えており、プログラミングを日々勉強中。</li>
+                <li>Hobby:旅行、スポーツ観戦、Runnning、飲酒(Beer、Whisky)</li>                
+            </ul>
+        </section>
+        <div class="skills">
+            <a id="skills"></a>
+            <h2>skills</h2>
+                <div class="graph">
+                    <div class="html">
+                        <span class="bar" style="width:70%;">HTML/CSS</span>
+                    </div>
+                    <div class="PHP">
+                            <span class="bar" style="width:20%;">PHP</span>
+                    </div>
+                    <div class="JQuery">
+                        <span class="bar" style="width:15%;">JQuery</span> 
+                    </div>
+                    <div class="Passion">
+                        <span class="bar" style="width:100%;">Passion</span>
+                    </div>
+                </div>
+            </div>
+        <div class="works">
+            <a id="works"></a>
+            <h2>Works</h2>
+            <p>No Image</p>
+        </div>
+        </main>
+    </body> 
+</html>
+       
+
+
